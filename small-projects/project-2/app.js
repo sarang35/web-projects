7ed8c55ebd302f85b438fbe1d8d4f9887ea64d40
sarang35 <sarangkrishnams@gmail.com>
2025-12-28 19:32:23 +0530

    corrected rippling effect

--- small-projects/project-2/app.js
+++ small-projects/project-2/app.js
@@ -1,10 +1,19 @@
-const btnEl=document.querySelector("btn")
+const btnEl = document.querySelector(".btn");
 
-btnEl.addEventListener("click", (e) => {
-  const x = e.pageX - btnEl.offsetLeft;
-  const y = e.pageY - btnEl.offsetTop;
+btnEl.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btnEl.offsetLeft;
+  const y = event.pageY - btnEl.offsetTop;
 
   btnEl.style.setProperty("--xPos", x + "px");
   btnEl.style.setProperty("--yPos", y + "px");
 });
 
+// const btnEl = document.querySelector(".btn");
+
+// btnEl.addEventListener("mouseover", (event) => {
+//   const x = event.pageX - btnEl.offsetLeft;
+//   const y = event.pageY - btnEl.offsetTop;
+
+//   btnEl.style.setProperty("--xPos", x + "px");
+//   btnEl.style.setProperty("--yPos", y + "px");
+// });
